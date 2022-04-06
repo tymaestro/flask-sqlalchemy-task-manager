ee44843331d575718f5fe2bd18a8ac652049fce7
tymaestro <tymaestro22@gmail.com>
2022-04-06 12:06:53 +0000

    add files

---
+++ taskmanager/__init__.py
@@ -0,0 +1,14 @@
+import osfrom
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECREY_KEY")
+app.config["SQLALCHEMY_DATABASE_URL"] = os.environ.get("DB_URL")
+
+db = SQLAlchemy(app)
+
+from taskmanager import routes
